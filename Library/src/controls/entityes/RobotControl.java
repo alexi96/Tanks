09b2a685bc56faa81767b3497b4aee38f04cd1ae
alexi96 <alex_i96@yahoo.com>
2017-02-26 11:02:12 +0200

    Ma apuc de robot
    Schimbat un task

---
+++ Library/src/controls/entityes/RobotControl.java
@@ -0,0 +1,17 @@
+package controls.entityes;
+
+import controls.GameControl;
+
+/**
+ * Pentru testarea modelului
+ */
+public class RobotControl extends GameControl {
+
+    public void create() {
+        
+    }
+    
+    @Override
+    public void update(float tpf) {
+    }
+}
