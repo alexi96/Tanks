6cf9c18ada3c45fc58ac28633904c4f914ba5658
alexi96 <alex_i96@yahoo.com>
2017-03-26 13:42:31 +0300

    S

--- Client/src/visual/WeaponInfo.java
+++ Client/src/visual/WeaponInfo.java
@@ -1,6 +1,32 @@
 package visual;
 
+import controls.weapons.WeaponControl;
+import java.awt.Graphics;
 
-public class WeaponInfo {
-    
+
+public class WeaponInfo extends Component {
+    private WeaponControl weapon;
+
+    public WeaponInfo() {
+    }
+
+    public WeaponInfo(int x, int y) {
+        super(x, y);
+    }
+
+    public WeaponInfo(int x, int y, int w, int h) {
+        super(x, y, w, h);
+    }
+
+    public WeaponControl getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(WeaponControl weapon) {
+        this.weapon = weapon;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+    }
 }
