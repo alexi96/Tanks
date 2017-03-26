cdb2937a3b9a026fe358a43e1bbd16e6fab0befd
delgado2009 <catal.warrior2@gmail.com>
2017-03-26 13:47:37 +0300

    S

--- Client/src/visual/SpawnFrame.java
+++ Client/src/visual/SpawnFrame.java
@@ -8,6 +8,7 @@ import java.awt.Graphics;
 public class SpawnFrame extends Frame {
     
     final private VehicleInfo vehicleInfo = new VehicleInfo();
+    final private WeaponInfo weaponInfo = new WeaponInfo();
 
     private final Button nextVehicle = new Button(">") {
         @Override
@@ -37,12 +38,14 @@ public class SpawnFrame extends Frame {
         this.lastVehicle.bounds(0, 0, buttonSize, buttonSize);
         this.nextVehicle.bounds(super.width()-buttonSize, 0, buttonSize, buttonSize);
         this.vehicleInfo.bounds(buttonSize, 0, super.width() - buttonSize*2, buttonSize);
+        this.weaponInfo.bounds(buttonSize, buttonSize, super.width() - buttonSize*2, buttonSize);
         this.lastVehicle.setFont(this.lastVehicle.getFont().deriveFont((float) buttonSize));
         this.nextVehicle.setFont(this.nextVehicle.getFont().deriveFont((float) buttonSize));
 
         super.add(this.nextVehicle);
         super.add(this.vehicleInfo);
         super.add(this.lastVehicle);
+        super.add(this.weaponInfo);
     }
 
     private void nextVehicle() {
