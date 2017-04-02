26a93e68a85b635a50ac4c0f1f7ef8404845d845
delgado2009 <catal.warrior2@gmail.com>
2017-04-02 18:37:54 +0300

    P

--- Library/src/controls/props/PlankControl.java
+++ Library/src/controls/props/PlankControl.java
@@ -1,6 +1,29 @@
 package controls.props;
 
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Spatial;
+import controllers.GameController;
+import controls.DestroyableControl;
+import synchronization.SyncManager;
 
-public class PlankControl {
+
+public class PlankControl extends BoxControl {
+    
+    public void create() {
+        Spatial plank = BoxControl.PROPS.getChild("Plank").clone();
+
+        plank.setLocalTranslation(this.location);
+        plank.setLocalRotation(this.rotation);
+        
+        SyncManager sm = GameController.getInstance().getSynchronizer();
+        if (sm == null) {
+            plank.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+        }
+
+        plank.addControl(this);
+
+        GameController.getInstance().getApplication().getRootNode().attachChild(plank);
+    }
+    
     
 }
